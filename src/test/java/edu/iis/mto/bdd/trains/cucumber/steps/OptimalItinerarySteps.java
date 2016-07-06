9cebd63b449a4ea36c619b63d165cc535193e64b
pawelek <p.dawiduk@gmail.com>
2016-07-07 00:09:42 +0200

    zmiany w optimalitinerarysteps

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -1,31 +1,44 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
-import java.util.List;
-
-import org.joda.time.LocalTime;
-
 import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
+import edu.iis.mto.bdd.trains.services.ItineraryService;
+import org.joda.time.LocalTime;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class OptimalItinerarySteps {
+	InMemoryTimetableService timetableService = new InMemoryTimetableService();
+	ItineraryService itineraryService = new ItineraryService(timetableService);
+
+	private String departure;
+	private String destination;
+	private LocalTime startTime;
 
 	@Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
 	public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
-			@Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
+									@Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
 		throw new PendingException();
 
 	}
 
 	@Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
 	public void whenIWantToTravel(String departure, String destination, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-		throw new PendingException();
+		this.departure = departure;
+		this.destination = destination;
+		this.startTime = startTime;
 	}
 
 	@Wtedy("^powinienem uzyskać informację o pociągach o:$")
 	public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-		throw new PendingException();
+		List<LocalTime> actualTrainTimes = itineraryService.findNextDepartures(departure, destination, startTime);
+		assertThat(actualTrainTimes, equalTo(expectedTrainTimes));
 	}
 }
